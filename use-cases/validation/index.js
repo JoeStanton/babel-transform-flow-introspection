5fa086409ab3b924960b9a7cb648270853bb1f18
Joe Stanton <joe.stanton@red-badger.com>
2016-10-03 00:00:52 -0500

    Switch to joi for validations

--- use-cases/validation/index.js
+++ use-cases/validation/index.js
@@ -1,113 +1,42 @@
 import range from "lodash.range";
+import joi from "joi";
+
 import * as Exports from "../types";
 
-const assertType = (type, input) => {
-  const {valid, errors} = validateType(type, input);
-  if (!valid) {
-    throw new Error(`Type validation: ${errors}`);
-  }
-  return true;
+const validateString = () => joi.string();
+const validateNum = () => joi.number();
+const validateBool = () => joi.boolean();
+const validateVoid = () => joi.valid(void 8);
+const validateLiteral = (def) => joi.valid(def.value);
+const validateUnion = (def) => joi.only(def.types.map(x => x.value));
+
+const validateObject = (def) => {
+  return joi.object(def.properties.reduce((acc, prop) => {
+      acc[prop.key.name] = generateSchema(prop.value);
+      return acc;
+    }, {})
+  );
 }
 
-const validate = (f, val, name) => {
-  if (!f(val)) {
-    return {
-      valid: false,
-      errors: `${JSON.stringify(val)} is not a ${name}`
-    }
-  }
-
-  return {
-    valid: true
-  }
-}
-const validateString = (_, val) => validate(v => (typeof v === "string"), val, "string");
-const validateNum = (_, val) => validate(v => (typeof v === "number"), val, "number");
-const validateBool = (_, val) => validate(v => (v === true || v === false), val, "bool");
-
-const validateArrayOf = (def, arr) => {
+const validateArrayOf = (def) => {
   const typeParam = def.typeParameters && def.typeParameters.params && def.typeParameters.params[0];
 
-  if (!Array.isArray(arr)) {
-    return {
-      valid: false,
-      errors: `${JSON.stringify(arr)} is not an array`
-    };
+  if (!typeParam) {
+    return joi.array();
   }
 
-  if (typeParam && arr.map(x => validateType(typeParam, x).valid).indexOf(false) !== -1) {
-    return {
-      valid: false,
-      errors: `One or more items in ${JSON.stringify(arr)} is an incorrect type`
-    }
-  }
-
-  return {
-    valid: true
-  }
-};
-const validateVoid = (_, v) => v === void 8;
-const validateLiteral = (def, val) => {
-  if (val === def.value) {
-    return { valid: true }
-  }
-
-  return {
-    valid: false,
-    errors: `${JSON.stringify(val)} is not equal to ${JSON.stringify(def.value)}`
-  }
-}
-
-const validateUnion = (def, val) => {
-  const values = def.types.map(x => x.value);
-
-  if (values.indexOf(val) !== -1) {
-    return { valid: true }
-  }
-
-  return {
-    valid: false,
-    errors: `${JSON.stringify(val)} is not one of: ${values.map(JSON.stringify).join(",")}`
-  }
+  return joi.array().items(generateSchema(typeParam));
 };
 
-const validateObject = (def, obj) => {
-  if (typeof obj !== "object") {
-    return {
-      valid: false,
-      errors: `${JSON.stringify(obj)} is not an object`
-    }
-  }
-
-  const props = def.properties.reduce((acc, prop) => {
-    acc[prop.key.name] = validateType(prop.value, obj[prop.key.name]);
-    return acc;
-  }, {});
-
-  const errors = Object.keys(props).filter(k => !props[k].valid)
-                                   .reduce((acc, k) => acc.concat([`${k} : ${props[k].errors}`]), []);
-
-  if (errors.length > 0) {
-    return {
-      valid: false,
-      errors
-    }
-  }
-
-  return {
-    valid: true
-  }
-}
-
-const validateGeneric = (def, object) => {
+const validateGeneric = (def) => {
   const exportedName = def.id.name + "Type";
 
   if (Exports[exportedName]) {
-    return validateType(Exports[exportedName], object);
+    return generateSchema(Exports[exportedName]);
   }
 
   if (validators[def.id.name]) {
-    return validators[def.id.name](def, object);
+    return validators[def.id.name](def);
   }
 
   throw new Error("No type declaration found for " + def.id.name);
@@ -125,24 +54,34 @@ const validators = {
   "NumberTypeAnnotation": validateNum,
   "BooleanTypeAnnotation": validateBool,
   "UnionTypeAnnotation": validateUnion,
-  "VoidTypeAnnotation": validateVoid,
+  "VoidTypeAnnotation": (def) => joi.valid(void 8),
   "Array": validateArrayOf,
 }
 
-const validateType = (type, input) => {
-  return validators[type.type](type, input);
+const generateSchema = (type) => {
+  return validators[type.type](type);
 }
 
-console.log(JSON.stringify(validateType(Exports.SuitType, "Diamond"), null, 2));
-console.log(JSON.stringify(validateType(Exports.AdministratorType, "Administrator"), null, 2));
-console.log(JSON.stringify(validateType(Exports.PropsType, {
+const validate = (value, type) => {
+  if (!type) {
+    throw new Error("Invalid type " + type);
+  }
+
+  return joi.validate(value, generateSchema(type), {
+    abortEarly: false
+  });
+}
+
+console.log(validate("Diamonds", Exports.SuitType));
+console.log(validate("Administrator", Exports.AdministratorType));
+console.log(validate({
   "users": [
     {
       "id": 1,
       "firstName": "Joe",
       "lastName": "String",
       "role": "Administrator",
-      "activated": true,
+      "activated": false,
       "profilePic": {
         "url": "String",
         "width": 1,
@@ -150,4 +89,4 @@ console.log(JSON.stringify(validateType(Exports.PropsType, {
       }
     }
   ]
-}), null, 2));
+}, Exports.PropsType));
