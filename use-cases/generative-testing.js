44ad81d7716e9984a578da01eee3859992a20906
Joe Stanton <joe.stanton@red-badger.com>
2016-10-03 00:23:29 -0500

    Comments

--- use-cases/generative-testing.js
+++ use-cases/generative-testing.js
@@ -1,7 +1,7 @@
 import * as Exports from "./types";
 import {setup} from "../flow-runtime/gen";
 
-const {gen, genN} = setup(Exports);
+const {gen, genN} = setup(Exports) // The generator must be able to recursively generate types;
 
 console.log(JSON.stringify(gen(Exports.LambdaContextType), null, 2));
 console.log(JSON.stringify(gen(Exports.PropsType), null, 2));
